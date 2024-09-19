4d2d13a7f92a214bda209ace78ce4027bcb088ef
SUDEEP M SHETTY <86517389+SUDEEP-M-SHETTY@users.noreply.github.com>
2024-09-19 15:59:28 +0530

    Update App.js

--- food-product-explorer/src/App.js
+++ food-product-explorer/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Routes from './routes';
 import AppRoutes from './routes'; // Import the routes
 import './App.css'; // Import your styles
 
 const App = () => {
-  return (
-    <div>
-      <h1>Food Product Explorer</h1>
-      <AppRoutes /> {/* Use the routes here */}
-    </div>
-  );
+    return (
+        <div>
+            <h1>Food Product Explorer</h1>
+            <AppRoutes /> {
+              /* Use the routes here */
+              <Navbar />
+              <Routes />
+            }
+        </div>
+    );
 };
 
 export default App;
+
