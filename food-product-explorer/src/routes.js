df3f6f60c8f88a5e99aab639821f2c6f2ad87d52
SUDEEP M SHETTY <86517389+SUDEEP-M-SHETTY@users.noreply.github.com>
2024-09-19 15:56:26 +0530

    Update routes.js

--- food-product-explorer/src/routes.js
+++ food-product-explorer/src/routes.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ProductDetailPage from './pages/ProductDetailPage';
-import NotFound from './pages/NotFound'; // Create this page if you haven't
+import NotFound from './pages/NotFound'; // Ensure this component is created
 
 const AppRoutes = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:barcode" element={<ProductDetailPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/product/:barcode" element={<ProductDetailPage />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default AppRoutes;
