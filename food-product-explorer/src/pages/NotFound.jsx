a53079497a34c6cd49372035d16c3bca677e8391
SUDEEP M SHETTY <86517389+SUDEEP-M-SHETTY@users.noreply.github.com>
2024-09-19 16:05:57 +0530

    Update NotFound.jsx

--- food-product-explorer/src/pages/NotFound.jsx
+++ food-product-explorer/src/pages/NotFound.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const NotFound = () => {
-  return <h1>404 - Not Found</h1>;
+    return (
+        <div style={{ textAlign: 'center', margin: '20px' }}>
+            <h2>404 - Not Found</h2>
+            <p>The page you are looking for does not exist.</p>
+        </div>
+    );
 };
 
 export default NotFound;
