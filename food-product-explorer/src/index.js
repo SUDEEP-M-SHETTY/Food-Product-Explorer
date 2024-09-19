d182daf3910b1bc1cbb18e322b8d4fe7b637676f
SUDEEP M SHETTY <86517389+SUDEEP-M-SHETTY@users.noreply.github.com>
2024-09-19 15:14:10 +0530

    Update index.js

--- food-product-explorer/src/index.js
+++ food-product-explorer/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'; // Make sure this path is correct
 import App from './App';
+import { ProductProvider } from './context/ProductContext'; // Import the provider
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ProductProvider>
+      <App />
+    </ProductProvider>
   </React.StrictMode>
 );
 
